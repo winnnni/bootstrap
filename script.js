fcfb82c9eb0f287534e82dab5f62001d71dacf77
winnnni <jz2521@columbia.edu>
2014-04-08 12:26:27 -0400

    changed the video embedded setting

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 /**
  * @author
  */
+
+var video ="<iframe width='560' height='315' src='//www.youtube.com/embed/o2-RXqU4Lg8' frameborder='0' allowfullscreen></iframe>";
+
+S(document).ready(function() {
+	setNAv();
+});
+
+function setNav(){
+	$(".cyclone").on("click", function() {
+		$('#myModal').modal();	
+		$('.modal-body').html(video);
+	});
+	
+	$(".close").on("click", function(){
+		$('.modal-body').empty();
+	});
+}
